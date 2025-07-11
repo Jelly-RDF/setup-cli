16d1514ba0cc3ba4cc91fbba2472e2cb37d9dfe3
Kacper Grzymkowski <kacper.grzymkowski@neverblink.eu>
2025-07-11 10:15:33 +0200

    Add support for Windows, macOS and Linux ARM (#10)
    Closes #2
    
    I moved out the unix-like installation script to its own file and added handling for translating os and architecture names, as well as moved the installation location to the runner tool cache since macOS was complaining.
    I also added a windows Powershell script that handles Windows installations.
    And tests for the newly supported systems: Ubuntu ARM, macOS, and Windows.

---
+++ unix-like.sh
@@ -0,0 +1,20 @@
+set -e
+if [ $INPUT_VERSION = latest ]; then
+    version=$(gh api repos/Jelly-RDF/cli/releases/latest -q .tag_name); 
+else 
+    version=$INPUT_VERSION; 
+    if [ ${version:0:1} != 'v' ]; then version="v$version"; fi
+fi
+if [ $RUNNER_OS = 'Linux' ]; then
+    osname="linux";
+else 
+    osname="mac";
+fi
+arch=$(uname -m)
+if [ $arch = "aarch64" ]; then
+    arch="arm64"
+fi
+mkdir "$RUNNER_TOOL_CACHE/jelly-cli"
+wget -O "$RUNNER_TOOL_CACHE/jelly-cli/jelly-cli" "https://github.com/Jelly-RDF/cli/releases/download/$version/jelly-cli-$osname-$arch"
+chmod +x "$RUNNER_TOOL_CACHE/jelly-cli/jelly-cli"
+echo "$RUNNER_TOOL_CACHE/jelly-cli" >> $GITHUB_PATH
