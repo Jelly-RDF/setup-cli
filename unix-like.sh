73238202d86a2448241b825bac3c0513fc4496fb
niegrzybkowski <kacper.grzymkowski@neverblink.eu>
2025-07-11 09:05:51 +0200

    Add support for arm and mac

--- unix-like.sh
+++ unix-like.sh
@@ -5,6 +5,12 @@ else
     version=$INPUT_VERSION; 
     if [ ${version:0:1} != 'v' ]; then version="v$version"; fi
 fi
-wget -O /opt/jelly-cli "https://github.com/Jelly-RDF/cli/releases/download/$version/jelly-cli-linux-x86_64"
+if [ $RUNNER_OS = 'Linux' ]; then
+    osname="linux";
+else 
+    osname="mac";
+fi
+arch=$(uname -m)
+wget -O /opt/jelly-cli "https://github.com/Jelly-RDF/cli/releases/download/$version/jelly-cli-$osname-$arch"
 chmod +x /opt/jelly-cli
 echo '/opt' >> $GITHUB_PATH
